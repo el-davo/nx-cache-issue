d7795c5277e7b8062270b2b117947cbdd9277a06
ahernd2 <david.ahern@dell.com>
2024-02-27 16:59:28 +0000

    Updated

--- libs/cache-bust/src/lib/cache-bust/cache-bust.component.ts
+++ libs/cache-bust/src/lib/cache-bust/cache-bust.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './cache-bust.component.css',
 })
 export class CacheBustComponent {
-  test = 'hdfghdfghdfghdgf';
+  test = 'xcvbxvcbxcvb';
 }
